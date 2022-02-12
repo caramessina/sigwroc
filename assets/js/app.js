4b3b42cea8d010b63c2bf15637b2f62e270e5e86
Chris Lindgren <chris.a.lindgren@gmail.com>
2022-02-12 18:32:16 -0500

    app js login

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,13 @@ import 'lazysizes';
 
 // global alert
 import './assets/js/alert';
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
